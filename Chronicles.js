d54164ab916862712d8ecf9f6303aa66867e5bb2
RTemiy <RTemiyGo@yandex.ru>
2023-03-12 15:33:26 +0300

    настройка гита

--- Chronicles.js
+++ Chronicles.js
@@ -2874,7 +2874,7 @@ Game.Scenes.A_Part01[42] =
 `,
     buttontext: [''],
     buttonaction: [() => { Game.Scenes.A_Part01[43].Begin(); }],
-    background: 'Backgrounds/Near_Lighthouse',
+    background: 'Backgrounds/Near_Lighthous',
   });
 
 Game.Scenes.A_Part01[43] =
