126a71db0f507b80e2d41f0a826dfde436a348b6
RTemiy <RTemiyGo@yandex.ru>
2023-04-20 02:11:15 +0300

    sw

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 var GHPATH = '/Chronicles';
 var APP_PREFIX = 'chronicles_';
-var VERSION = 'version_02.2';
+var VERSION = 'version_02.21';
+var CACHE_NAME = APP_PREFIX + VERSION;
 
 var URLS = [
 
@@ -204,42 +205,50 @@ var URLS = [
   `${GHPATH}/sounds/Lake.mp3`,
 ];
 
-
-var CACHE_NAME = APP_PREFIX + VERSION
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('Responding with cache : ' + e.request.url);
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
         return request
-      } else {
-        console.log('File is not cached, fetching : ' + e.request.url);
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   )
 })
 
+// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
+      console.log('installing cache : ' + CACHE_NAME)
       return cache.addAll(URLS)
     })
   )
 })
 
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
       })
-      cacheWhitelist.push(CACHE_NAME);
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i] );
+          console.log('deleting cache : ' + keyList[i] )
           return caches.delete(keyList[i])
         }
       }))
